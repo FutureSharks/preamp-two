12f97b410069185621e893c8ebee5d3c117a40ea
Max Williams <futuresharks@gmail.com>
2018-12-13 20:15:48 +0100

    finished circuitpython test code for input selector

--- code/test_code/input-selector-tester/circuit-python-test.py
+++ code/test_code/input-selector-tester/circuit-python-test.py
@@ -16,15 +16,57 @@ def set_reg(register, value):
         d.write(bytearray([value]))
 
 
+def set_gio(gio_a, gio_b):
+    with device as d:
+        d.write(bytearray([64]))
+        d.write(bytearray([18]))
+        d.write(bytearray([gio_a]))
+        d.write(bytearray([gio_b]))
+
+
+def reset_relays():
+    set_gio(128, 0)
+
 print('init')
 set_reg(0, 0)
+set_reg(1, 0)
+
+time_delay = 1
 
 while True:
-    time.sleep(1)
-    # unmute
-    print('unmute')
-    set_reg(18, 0)
-    time.sleep(1)
-    # mute
-    print('mute')
-    set_reg(18, 147)
+    print('Switching relay 1')
+    set_gio(213, 72)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay 2')
+    set_gio(213, 48)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay 3')
+    set_gio(212, 208)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay 4')
+    set_gio(211, 80)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay 5')
+    set_gio(205, 80)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay 6')
+    set_gio(181, 80)
+    time.sleep(0.01)
+    reset_relays()
+    time.sleep(time_delay)
+    print('Switching relay mute on')
+    set_gio(0, 0)
+    time.sleep(time_delay)
+    print('Switching relay mute off')
+    reset_relays()
+    time.sleep(time_delay)
